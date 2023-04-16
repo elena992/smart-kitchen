d92379f9c604156248862f4ed5701c13ba5968f0
elena992 <elena.fernandez1992@gmail.com>
2023-04-16 17:32:16 +0200

    wip

--- API/controllers/recipes.controller.js
+++ API/controllers/recipes.controller.js
@@ -1,6 +1,7 @@
 const Recipe = require("../models/Recipe.model");
 const createError = require("http-errors");
 const { StatusCodes } = require("http-status-codes");
+const axios = require('axios');
 
 module.exports.create = (req, res, next) => {
   if (req.file) {
@@ -32,4 +33,32 @@ module.exports.created = (req, res, next) => {
   Recipe.findByIdAndUpdate(id, { createdBy: req.currentUserId }, { new: true })
     .then((recipe) => res.json(recipe))
     .catch(next);
+};
+
+module.exports.searchRecipes = (req, res, next) => {
+  const prompt = `Give me some recipe ideas using the following ingredients: ${req.body.ingredients}.`;
+  const data = {
+    prompt: prompt,
+    model: `text-davinci-003`,
+    max_tokens: 60,
+    n: 1,
+    stop: "\n",
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  };
+  axios.post("https://api.openai/v1/completions", data, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+    })
+    .then((response) => {
+      const result = response.data.choices[0].text.trim();
+      res.json({ recipes: result });
+    })
+    .catch((error) => {
+      next(error)
+    });
 };
--- WEB/smart-kitchen-web/src/views/SearchRecipes/SearchRecipes.jsx
+++ WEB/smart-kitchen-web/src/views/SearchRecipes/SearchRecipes.jsx
@@ -1,4 +1,18 @@
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
 
+/*const [data, setData] = useState([]);
+
+useEffect(() => {
+    axios.get('https://api.openai.com')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+  
 
 const SearchRecipes = () => {
 
@@ -11,6 +25,98 @@ const SearchRecipes = () => {
       </div>
 
     )
-}
+} */
 
-export default SearchRecipes;
+/*
+function SearchRecipes() {
+  const [ingredients, setIngredients] = useState('');
+  const [response, setResponse] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  const handleInputChange = (event) => {
+    setIngredients(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Buscando recetas para:', ingredients);
+  };
+
+  return (
+    <div className= "container">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Type your ingredients to get your recipe
+          <input type="text" value={ingredients} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {response && <p>{response}</p>}
+    </div>
+  );
+}
+*/
+function SearchRecipes() {
+    const [ingredients, setIngredients] = useState('');
+    const [recipes, setRecipes] = useState([]);
+    const [error, setError] = useState(null);
+  
+    const handleInputChange = (event) => {
+      setIngredients(event.target.value);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log('Buscando recetas para:', ingredients);
+     
+      fetch('https://api.openai.com/v1/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({ ingredients: ingredients })
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data && data.recipes) {
+          setRecipes(data.recipes);
+        } else {
+          setError('Invalid response from API');
+        }
+      })
+      .catch(error => {
+        setError('Error fetching data: ' + error);
+      });
+    };
+  
+    return (
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Type your ingredients to get your recipe
+            <input type="text" value={ingredients} onChange={handleInputChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        {error && <p className="error">{error}</p>}
+        <div className="recipe-cards">
+          {recipes.map(recipe => (
+            <div key={recipe.id} className="recipe-card">
+              <h2>{recipe.title}</h2>
+              <img src={recipe.image} alt={recipe.title} />
+              <ul>
+                {recipe.ingredients.map(ingredient => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+              </ul>
+              <p>{recipe.instructions}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
+  export default SearchRecipes;
+
