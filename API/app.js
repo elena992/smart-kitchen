6279a2a58584d3668b4645244b57999ea63dc6a3
elena992 <elena.fernandez1992@gmail.com>
2023-04-22 14:24:21 +0200

    branch just api

--- API/app.js
+++ API/app.js
@@ -13,7 +13,7 @@ require("./config/db.config");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   })
 );
 
--- WEB/src/services/AuthService.js
+++
@@ -1,7 +0,0 @@
-import { createHttp } from './BaseService';
-
-const http = createHttp(false);
-
-export const login = ({ email, password }) => http.post('/login', { email, password })
-
-export const signup = ({firstName, lastName, restaurantName, email, password}) => http.post('/signup', {firstName, lastName, restaurantName, email, password})
