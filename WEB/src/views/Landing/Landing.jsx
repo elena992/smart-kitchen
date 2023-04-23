6d1d185d585b0fb7ecbb2e9a954fc5647105de56
elena992 <elena.fernandez1992@gmail.com>
2023-04-23 14:11:55 +0200

    landing wip

--- WEB/src/views/Landing/Landing.jsx
+++ WEB/src/views/Landing/Landing.jsx
@@ -1,13 +1,72 @@
-import './Landing.css'
+import "./Landing.css";
+import AuthContext from "../../contexts/AuthContext";
+import { useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 const Landing = () => {
-    return (
-        <div className="landing-page">
-          <div className="landing-page-content">
-            <h1>Welcome to My Website</h1>
+  const { currentUser } = useContext(AuthContext);
+  const Navigate = useNavigate();
+
+  return (
+    <div className="landing-page">
+      <div className="landing-page-content">
+        {!currentUser ? (
+          <div>
+            <h1>Welcome to Smart Kitchen</h1>
+            <div className="wrapper">
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  window.location.href = "/login";
+                }}
+              >
+                Log in
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  window.location.href = "/signup";
+                }}
+              >
+                Sign up
+              </button>
+            </div>
           </div>
-        </div>
-      );
+        ) : (
+          <div>
+            <h1>Welcome {currentUser.firstName} to Smart Kitchen</h1>
+            <h3>What do you want to do today?</h3>
+            <div className="wrapper">
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  window.location.href = "/create-recipe";
+                }}
+              >
+                Create
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  window.location.href = "/recipes/search";
+                }}
+              >
+                Search
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  window.location.href = "/my-recipes";
+                }}
+              >
+                My Recipes
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
