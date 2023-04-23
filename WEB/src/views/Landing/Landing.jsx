f324709ace0efc897c58076e1483ada6a8d0854b
elena992 <elena.fernandez1992@gmail.com>
2023-04-23 21:15:18 +0200

    logo in landing

--- WEB/src/views/Landing/Landing.jsx
+++ WEB/src/views/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import "./Landing.css";
 import AuthContext from "../../contexts/AuthContext";
 import { useContext } from "react";
 import { logout } from "../../stores/AccessTokenStore.js";
+import logo from "../../assets/images/logo.png";
 
 const Landing = () => {
   const { currentUser } = useContext(AuthContext);
@@ -18,6 +19,9 @@ const Landing = () => {
       <div className="landing-page-content">
         {!currentUser ? (
           <div>
+            <div>
+              <img src={logo} alt="Logo" className="logo" />
+            </div>
             <h1>Welcome to Smart Kitchen</h1>
             <div className="wrapper">
               <button
@@ -40,6 +44,9 @@ const Landing = () => {
           </div>
         ) : (
           <div>
+            <div>
+              <img src={logo} alt="Logo" className="logo" />
+            </div>
             <h1>Welcome {currentUser.firstName} to Smart Kitchen</h1>
             <h3>What do you want to do today?</h3>
             <div className="wrapper">
